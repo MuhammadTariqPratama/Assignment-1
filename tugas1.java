e0d0889b969b47a5458db4a46266c0a9eff2c7f3
MuhammadTariqPratama <147516436+MuhammadTariqPratama@users.noreply.github.com>
2023-11-04 22:18:13 +0700

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,13 @@
+package Tugas;
+
+public class tugas1 {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++){
+            if (i <=9){
+                System.out.println(i);
+            }else{
+                System.out.println("Tariq");
+            }
+        }
+    }
+}
